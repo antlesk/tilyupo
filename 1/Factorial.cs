1aacae76a106b278a46fa65447193c934bf9dd9c
tilyupo <tilyupo@gmail.com>
2017-02-18 19:40:53 +0300

    Added a new lab (1)

---
+++ 1/Factorial.cs
@@ -0,0 +1,29 @@
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public static class Factorial
+    {
+        private static List<BigInteger> _memory;
+
+        static Factorial()
+        {
+            _memory = new List<BigInteger>();
+            _memory.Add(new BigInteger(1));
+        }
+
+        public static BigInteger Get(int n)
+        {
+            if (n > _memory.Count - 1)
+            {
+                for (int i = _memory.Count; i <= n; i++)
+                {
+                    _memory.Add(_memory[i - 1] * (new BigInteger(i)));
+                }
+            }
+
+            return _memory[n];
+        }
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели недопустимое значение!");
+                return;
+            }
+            BigInteger res = new BigInteger(0);
+            for (int i = 1; i <= n; i++)
+            {
+                BigInteger curr = new BigInteger(1);
+                for (int j = 1; j <= i; j++)
+                {
+                    curr *= Factorial.Get(i + j) / Factorial.Get(i);
+                }
+                res += curr;
+            }
+            Console.WriteLine("Результат: " + res.ToString());
+        }
+    }
+}
